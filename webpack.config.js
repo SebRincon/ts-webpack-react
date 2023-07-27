8aba6933c152c136f0a4eb24882014385df547e2
seb4594 <65371156+seb4594@users.noreply.github.com>
2023-07-26 19:41:17 -0500

    Setup for multiple chunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 const HtmlPlugin = require('html-webpack-plugin')
-const { title } = require('process')
 
 module.exports = {
   mode: 'development',
+  devtool: 'cheap-module-source-map',
+  // Setup Chunks
   entry: {
     popup: path.resolve('src/popup/popup.tsx'),
+    options: path.resolve('src/options/options.tsx'),
   },
 
   module: {
@@ -23,23 +25,33 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve('src/manifest.json'),
+          from: path.resolve('src/static'),
           to: path.resolve('dist'),
         },
       ],
     }),
     // Using the HTML Plugin to copy over HTML to dist
-    new HtmlPlugin({
-      title: 'React Extension',
-      filename: 'popup.html',
-      chunks: ['popup'],
-    }),
+    ...getHtmlPlugins(['popup', 'options']),
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
+  // Output will use chunk name
   output: {
     filename: '[name].js',
     path: path.resolve('dist'),
   },
+  // Splitting chunks, allowing chunks to share modules -> Helps save space
+  optimization: { splitChunks: {chunks: 'all'} },
+}
+
+
+
+// Using this function to generate HTML plugins for each chunk
+function getHtmlPlugins(chunks) { 
+  return chunks.map(chunk => new HtmlPlugin({
+    title: 'React Extension',
+    filename: `${chunk}.html`,
+    chunks: [chunk],
+  }))
 }
